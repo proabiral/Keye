b93bf63cf67870c6b70f01a484ce906cd390a068
Pro Abiral <22173232+proabiral@users.noreply.github.com>
2019-12-30 21:35:20 +0545

    notify on percentage change

--- keye.py
+++ keye.py
@@ -8,15 +8,27 @@ from slackconfig import *
 import json
 requests.packages.urllib3.disable_warnings()
 
+import sys
+if sys.version_info[0] < 3:
+    raise Exception("Must be using Python 3")
+
 parser = argparse.ArgumentParser()
 parser.add_argument('-s', '--singleurl', help='Single URL to add. E.g: http://google.com', dest='singleurl')
 parser.add_argument('-ul', '--urlslist', help='File with new urls to add. E.g: urls.txt', dest='urlslist')
 parser.add_argument('-rm', '--remove', help='URL to remove from database. E.g: http://google.com', dest='urltoremove')
 parser.add_argument('-d', '--display', help='Display all monitored URLs', required =  False, nargs='?', const="True", dest='displayurls')
-parser.add_argument('-x', '--add-conn-error', help='Add urls to db even when connection fails at the time of adding', required =  False,action='store_true', dest='addOnConnError')
+parser.add_argument('-x', '--add-conn-error', help='Add urls to db even when connection fails at the time of adding', required =  False, action='store_true', dest='addOnConnError')
+parser.add_argument('-p', '--req-percent-change',help='Percentage required to be changed in content-type for notification', required = False, default=1, type=float, dest='requiredPercentChange')
 
 args = parser.parse_args()
 
+def getContentLength(response):
+        responseHeaders = response.headers
+        if "content-length" in responseHeaders:
+            newcontentlength = responseHeaders["content-length"]
+        else:
+            newcontentlength = len(response.content)
+        return int(newcontentlength)
 
 def db_install():
     if (os.path.isfile('./keye.db')) == False:
@@ -62,7 +74,8 @@ def request(url):
     try:
         if not "http" in url:
             url = "http://" + url
-        contentlength = requests.get(url, allow_redirects=True, verify=False, timeout=5).headers['content-length']
+        response=requests.get(url, allow_redirects=True, verify=False, timeout=5)
+        contentlength = getContentLength(response)
         try:
             committodb(oURL, contentlength)
             print("We have successfully added " + url +" to be monitored.")
@@ -71,7 +84,8 @@ def request(url):
     except:
         try:
             url = url.replace("http://", "https://")
-            contentlength = requests.get(url, allow_redirects=True, timeout=5).headers['content-length']
+            response = requests.get(url, allow_redirects=True, timeout=5)
+            contentlength=getContentLength(response)
             committodb(oURL, contentlength)
             print("We have successfully added " + url +" to be monitored.")
         except Exception as e:
@@ -99,7 +113,7 @@ def main():
             for row in all_rows:
                 id = row[0]
                 url = row[1]
-                contentlength = str(row[2])
+                contentlength = int(row[2])
                 connect(id, url, contentlength)
     except Exception as e:
         print(e)
@@ -109,15 +123,27 @@ def connect(id, url, contentlength):
         url = 'http://'+url
 
     try:
-        newcontentlength = requests.get(url, allow_redirects=True, verify=False, timeout=5).headers['content-length']
-        if newcontentlength == contentlength:
-            pass
-        else:
-            notify(url)
+        response = requests.get(url, allow_redirects=True, verify=False, timeout=5)
+    except:
+        url = url.replace("http://", "https://")
+        try:
+            response = requests.get(url, allow_redirects=True, verify=False, timeout=5)
+        except Exception as e:
+            print (e)
+            return
+    newcontentlength = getContentLength(response)
+    if newcontentlength == contentlength:
+        print ("No change , URL " + url + " New "+str(newcontentlength)+" Old "+str(contentlength))
+        pass
+    else:
+        percentChange=(newcontentlength-contentlength)*100/contentlength
+        if abs(percentChange) > args.requiredPercentChange:
+            notify(url,contentlength,newcontentlength)
             cursor.execute('''UPDATE urls SET contentlength = ? WHERE id = ? ''', (newcontentlength, id))
             db.commit()
-    except Exception as e:
-        print("We could not connect to {} due to following error: {}".format(url, e))
+        else:   
+            print ("Some change , URL " + url + " NEW: "+str(newcontentlength)+", OLD: "+str(contentlength)+" Percent Change : " + str(abs(percentChange)) + " Notification not send as minimum threashold to notify not met. ")
+    
 
 def removefromdb():
     urltoremove = args.urltoremove
@@ -138,9 +164,9 @@ def displayurls():
         print("We couldn't retrieve URLs due to following error {}".format(e))
 
 
-def notify(url):
+def notify(url,previousCL,newCL):
     webhook_url = posting_webhook
-    slack_data = {'text': 'Changes detected on: ' + url}
+    slack_data = {'text': 'Changes detected on: ' + url +'. Previous CL: '+ str(previousCL) + ', New CL: ' + str(newCL)}
     sendnotification = requests.post(webhook_url, data=json.dumps(slack_data), headers={'Content-Type': 'application/json'})
 
 db_install()
